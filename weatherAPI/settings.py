44954350ea31d580c0ebf4d8d24a06c526bad076
jaycode8 <jamesmumo443@gmail.com>
2025-05-11 14:56:46 +0300

    feat(updates)

--- weatherAPI/settings.py
+++ weatherAPI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-2exe#st(q%d#)po#enx(wm9slcdmx55$nalk^af=se6v=vs5py
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*.vercel.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
