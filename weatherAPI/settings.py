bb6b2c2068c4653ce22d699c225d9ac339096e59
jaycode8 <jamesmumo443@gmail.com>
2025-05-11 14:46:06 +0300

    feat(updates)

--- weatherAPI/settings.py
+++ weatherAPI/settings.py
@@ -74,12 +74,12 @@ WSGI_APPLICATION = "weatherAPI.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 CACHES = {
     "default":{
